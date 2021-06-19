ef6175b544f9b87d3687916b96559a41e8694e35
Brandixitor <med.salah.bourouba@gmail.com>
2021-06-19 19:50:45 +0100

    Update

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -15,7 +15,10 @@ public class MainMenu : MonoBehaviour
   public void QuitGame ()
   {
     Debug.Log("Quit!");
-    Application.Quit();
+     if (Input.GetKeyDown(KeyCode.Escape))
+ { 
+      Application.Quit();
+ }
   }
 
 }
