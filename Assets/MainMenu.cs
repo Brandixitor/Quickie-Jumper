71234ad835662b2055ab93909693f988a0d351d2
Brandixitor <med.salah.bourouba@gmail.com>
2021-06-19 10:54:27 +0100

    Update

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -11,6 +11,11 @@ public class MainMenu : MonoBehaviour
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
   }
 
-
+//Function to quit the game upon clicking on QUIT.
+  public void QuitGame ()
+  {
+    Debug.Log("Quit!");
+    Application.Quit();
+  }
 
 }
