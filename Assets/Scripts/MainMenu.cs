9d4f4fd883fc565079d96f69ad006f0eac81ef1a
Brandixitor <med.salah.bourouba@gmail.com>
2021-06-20 12:25:20 +0100

    Update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+void Start()
+    {
+      // This will hopefully auto rotate the screen upon launch (doesn't work when ScreenRotate is off!!!)
+        Screen.autorotateToPortrait = true;
+
+        Screen.autorotateToPortraitUpsideDown = true;
+
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+
+
+
+
   //Function to start the game upon clicking on PLAY.
     public void PlayGame ()
   {
